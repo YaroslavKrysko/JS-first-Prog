ec06ee4dc7948f9789b9a7ab459714e210a7b1ae
Yaroslav Krysko <ytv7514@gmail.com>
2025-04-22 18:46:19 +0200

    Trim

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
 	for (let i = 0; i < 2; i++){
-	const a = prompt("Один из последних просмотрених фильмов?", ''),
+	const a = prompt("Один из последних просмотрених фильмов?", '').trim(),
 		b = prompt("На сколько оцените его?", '');
 	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
 		personalMovieDB.movies[a] = b;
